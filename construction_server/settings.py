ceef21591ea01e142182fed5a6c15cf1df2eee3b
kripas1369 <kripask50@gmail.com>
2023-07-01 23:37:21 +0545

    old code

--- construction_server/settings.py
+++ construction_server/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=&m^1637dkrse&c&i8lr=c*@g*c+qplx#or)@5-v&5hgb5=jd-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','meet1369.pythonanywhere.com']
 
 
 # Application definition
@@ -59,7 +60,7 @@ ROOT_URLCONF = "construction_server.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templetes')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT =os.path.join(BASE_DIR,'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
